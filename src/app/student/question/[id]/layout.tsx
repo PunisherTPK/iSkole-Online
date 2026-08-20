91339e3c798de8c8fb3e8f997c2703840c0ae85d
PunisherTPK <126077795+PunisherTPK@users.noreply.github.com>
2026-08-21 01:53:36 +0530

    fix: remove public navbar from question pages

--- src/app/student/question/[id]/layout.tsx
+++ src/app/student/question/[id]/layout.tsx
@@ -1,16 +1,3 @@
-import Navbar from "@/components/public/Navbar";
-import Footer from "@/components/public/Footer";
-
 export default function PublicQuestionPageLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Navbar />
-      <main className="min-h-[calc(100vh-72px)] bg-background">
-        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-          {children}
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
+  return <>{children}</>;
 }
